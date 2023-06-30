bba86431677718783c8122a611e8d2319938c0f5
Elias Rodrigues de Sousa <e.sousa@outlook.com.br>
2023-06-30 20:53:08 -0300

    Definindo interfaces padroes

---
+++ UsersAPI.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace UsersAPI.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity, Tkey> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Func<TEntity, bool> where);
+        TEntity? GetById(TEntity id);
+        TEntity? Get(Func<TEntity, bool> where);
+    }
+}
