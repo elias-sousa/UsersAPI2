38686bc816d9e02657520c3bfbc64eaa2efe0580
Elias Rodrigues de Sousa <e.sousa@outlook.com.br>
2023-06-30 19:10:59 -0300

    Criação do dominio e estrutura de testes e jwt

---
+++ UsersAPI.Tests/AuthTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UsersAPI.Tests
+{
+    public class AuthTests
+    {
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_Login_Return_ok() { 
+        
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_Login_Return_NaoAutorizado()
+        {
+
+        }
+
+
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_ResetarSenha_ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_ResetarSenha_NaoEncontrado()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_EsqueciMinhaSenha_ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Auth_Post_EsqueciMinhaSenha_NaoEncontrado()
+        {
+
+        }
+
+    }
+}
---
+++ UsersAPI.Tests/UsersTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UsersAPI.Tests
+{
+    public class UsersTests
+    {
+        [Fact(Skip = "Não Implementado")]
+        public void Users_Post_Return_ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Users_Post_Return_NaoEncontrado()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Users_Put_Return_ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Users_Delete_Return_ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não Implementado")]
+        public void Users_Get_Return_ok()
+        {
+
+        }
+
+
+    }
+}
